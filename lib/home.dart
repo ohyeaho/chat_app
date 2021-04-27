13b72615021381921070a5637bef256d92025939
ohyeaho <ohyeah7275@gmail.com>
2021-04-27 11:51:40 +0900

    home add

---
+++ lib/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'chat app',
+                style: TextStyle(fontSize: 55.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text('ログイン'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login_page');
+                },
+              ),
+              ElevatedButton(
+                child: Text('ユーザー登録'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/signup_page');
+                },
+              ),
+              ElevatedButton(
+                child: Text('chat'),
+                onPressed: () {
+                  //todo
+                  // Navigator.pushNamed(context, '/map');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
