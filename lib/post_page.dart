0832d76bff139c97ada5be383e13374f25429268
ohyeaho <ohyeah7275@gmail.com>
2021-04-28 18:03:30 +0900

    post_page add

---
+++ lib/post_page.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class PostPage extends StatefulWidget {
+  PostPage(this.user);
+  final User user;
+
+  @override
+  _PostPageState createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  String messageText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('post'),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: '投稿メッセージ'),
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+                onChanged: (String value) {
+                  setState(() {
+                    messageText = value;
+                  });
+                },
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  child: Text('投稿'),
+                  onPressed: () async {
+                    final date = DateTime.now().toLocal().toIso8601String();
+                    final email = widget.user.email;
+                    await FirebaseFirestore.instance
+                        .collection('posts')
+                        .doc()
+                        .set({
+                      'text': messageText,
+                      'email': email,
+                      'date': date
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
